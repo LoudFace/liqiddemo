232e0f5b4dc5a766ddc6f8483fc683d3fed41c51
“samLoudface” <138557926+SamV-vhibes@users.noreply.github.com>
2023-07-12 14:03:26 +0100

    Auth0 code implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,55 @@
-import { greetUser } from '$utils/greet';
+import { createAuth0Client } from '@auth0/auth0-spa-js';
+const init = async () => {
+  const client = await createAuth0Client({
+    clientId: '5de63480d4a1c7081dd4423a',
+    domain: 'auth.liqid.rocks',
+    authorizationParams: {
+      redirect_uri: 'https://arnels-ultra-awesome-site-8feb2b.webflow.io/',
+    },
+  });
+  console.log(client);
+  const hidebtn = document.getElementById('hideLink') as HTMLAnchorElement;
+  console.log(hidebtn);
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+  const url = new URLSearchParams(window.location.search);
+  const code = url.get('code');
+
+  if (code) {
+    await client.handleRedirectCallback();
+    history.replaceState({}, document.title, window.location.origin + window.location.pathname);
+  }
+
+  const isLoggedIn = await client.isAuthenticated();
+  if (!isLoggedIn) {
+    hidebtn.href = `http://nolink`;
+  }
+  console.log(isLoggedIn);
+
+  if (isLoggedIn) {
+    hidebtn.href = 'http://google.com';
+  }
+
+  console.log(client);
+
+  window.Webflow ||= [];
+  window.Webflow.push(() => {
+    //const name = 'Liqid';
+    // console.log('jj');
+
+    const signupBtn = document.getElementById('authSignup');
+    const logOutUser = document.getElementById('authLogout');
+    if (!signupBtn || !logOutUser) return;
+
+    signupBtn.addEventListener('click', async (e) => {
+      (await client).loginWithRedirect();
+      console.log(e);
+    });
+
+    logOutUser.addEventListener('click', async () => {
+      (await client).logout();
+    });
+    //console.log(name);
+  });
+};
+
+init();
